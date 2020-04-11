1a4c117c48e4d8947a8f87c3854456c68c817f44
0s4lv0 <buffasalvatore97@gmail.com>
2020-04-11 17:18:11 +0200

    update website

--- website/core/funzioni_mysql.php
+++ website/core/funzioni_mysql.php
@@ -1,17 +1,15 @@
 <?php
 
-$db = "************";
+$db = "my_progettomultisensory";
 $host = "localhost";
-$username = "***********";
-$password = "***********";
+$username = "progettomultisensory";
+$password = "ProgettoMulti123";
 
-if(!$connection = @mysql_connect($host,$username,$password))
-{
+if(!$connection = @mysql_connect($host,$username,$password)) {
 	echo 'Impossibile connettersi a MySql';
 	die;
 }
-if(!@mysql_select_db($db,$connection))
-{
+if(!@mysql_select_db($db,$connection)) {
 	echo "Impossibile selezionare il database ".$db;
 	die;
 }
